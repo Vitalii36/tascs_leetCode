1c529f6f2ef80f80917f583e07ce9436c711da11
Vitalii36 <vitaliipravdych@gmail.com>
2021-03-14 14:59:31 +0200

    66

---
+++ Python/Ex_66_Plus_One.py
@@ -0,0 +1,49 @@
+"""
+Given a non-empty array of decimal digits representing a non-negative integer,
+increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of
+ the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the
+number 0 itself.
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Example 3:
+
+Input: digits = [0]
+Output: [1]
+"""
+
+
+class Solution:
+    def plusOne(self, digits):
+        digits.reverse()
+        for i in range(0, len(digits)):
+            if digits[i] == 9 and i == len(digits)-1:
+                digits[i] = 0
+                digits.append(1)
+                digits.reverse()
+            elif digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                digits.reverse()
+                break
+        return digits
+
+
+result = Solution()
+
+digits = [9,9,9,9]
+
+print(result.plusOne(digits))
