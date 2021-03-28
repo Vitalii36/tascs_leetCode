11162a18b38bfec372d8c10c68f21e61e0594f33
Vitalii36 <vitaliipravdych@gmail.com>
2021-03-28 15:59:07 +0300

    Ex 108

---
+++ Python/Ex_108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -0,0 +1,47 @@
+"""
+Given an integer array nums where the elements are sorted in ascending order,
+ convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two
+subtrees of every node never differs by more than one.
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,3] and [3,1] are both a height-balanced BSTs.
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        def helper(l, r):
+            if l > r:
+                return None
+            m = (l + r) // 2
+            node = TreeNode(nums[m])
+            node.left = helper(l, m - 1)
+            node.right = helper(m + 1, r)
+            return node
+        return helper(0, len(nums) - 1)
+
+
+result = Solution()
+nums = [-10,-3,0,5,9]
+print(result.sortedArrayToBST(nums))
