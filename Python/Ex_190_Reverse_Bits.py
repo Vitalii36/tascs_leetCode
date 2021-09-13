374ce8733d84dec499384467b4f9551494dd6bde
vitalii <vitaliipravdych@gmail.com>
2021-09-13 22:49:11 +0300

    [UPD] Ex_191 Number of 1 Bits

--- Python/Ex_190_Reverse_Bits.py
+++ Python/Ex_190_Reverse_Bits.py
@@ -33,10 +33,11 @@ is 10111111111111111111111111111111.
 
 '''
 
+
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
         for i in reversed(range(32)):
-            res|=(n&1)<<i
-            n>>=1
+            res |= (n & 1) << i
+            n >>= 1
         return res
