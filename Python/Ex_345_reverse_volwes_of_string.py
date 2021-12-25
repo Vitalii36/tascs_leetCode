09ee6d567e0bb58b914fee5117a288a41720d094
vitalii <vitaliipravdych@gmail.com>
2021-12-25 21:24:50 +0200

    [UPD] Ex_345_reverse_volwes_of_string

---
+++ Python/Ex_345_reverse_volwes_of_string.py
@@ -0,0 +1,42 @@
+'''
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both cases.
+
+
+
+Example 1:
+
+Input: s = "hello"
+Output: "holle"
+Example 2:
+
+Input: s = "leetcode"
+Output: "leotcede"
+
+
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consist of printable ASCII characters.
+'''
+
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] in vowels and s[j] in vowels:
+                a, b = s[j], s[i]
+                s = s[:i] + a + s[i + 1:j] + b + s[j + 1:]
+                i += 1
+                j -= 1
+            elif s[i] in vowels:
+                j -= 1
+            elif s[j] in vowels:
+                i += 1
+            else:
+                i += 1
+                j -= 1
+        return s
