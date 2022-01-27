9cf04b550ed79471cdf12c01710fdf882164f4d1
Pravdych Vitalii <vitaliipravdych@gmail.com>
2022-01-27 09:21:28 +0200

    [UPD] time interval

--- Python/My_Ex_Time_interval.py
+++ Python/My_Ex_Time_interval.py
@@ -9,53 +9,101 @@ Out=1.00
 or
 6.00 - 14.00
 '''
-from datetime import datetime, timedelta
+
+
+        # if t1 < t2 and t3 > t4 and ((t3 <= t2 <= 24 or 0 <= t2 <= t4) or (t3 <= t1 <= 24 or 0 <= t1 <= t4)):
+        #     if t1 < t3 and t2 > t4:
+        #         if t2 > t3:
+        #             return t2 - t3
+        #         elif t4 > t1:
+        #             return 24 - t3 + t4
+        #         return 24 - t3 + t1
+        #     elif t1 > t3 and t2 > t4:
+        #         return t2 - t1
+        #     elif t1 < t3 and t2 < t4:
+        #         return t2 - t1
+        #
+        # elif t1 < t2 and t3 < t4 and ((t3 <= t2 <= t4) or (t3 <= t1 <= t4)):
+        #     if t1 >= t3 and t2 <= t4:
+        #         return t2 - t1
+        #     elif t1 <= t3 and t2 <= t4:
+        #         return t2 - t3
+        #     elif t1 >= t3 and t2 >= t4:
+        #         return t4 - t1
+        #     else:
+        #         t4 - t2
+        # elif t1 > t2 and t3 < t4 and ((t3 <= t2 <= 24 and 0 < t2 <= t4) or (t3 <= t1 <= 24 and 0 < t1 <= t4)):
+        #     if t2 >= t3 and t1 <= t4:
+        #         return t2 - t1
+        #     elif t2 >= t3 and t1 >= t4:
+        #         return t2 - t4
+        # elif t1 > t2 and t3 > t4:
+        #     if t2 <= t4 and t1 >= t3:
+        #         return 24 - t1 + t2
+        #     elif t2 <= t4 and t1 <= t3:
+        #         return 24 - t3 + t2
+        #     elif t2 >= t4 and t1 <= t3:
+        #         return 24 - t3 + t4
+        #     elif t2 >= t4 and t1 >= t3:
+        #         return 24 - t1 + t4
+
 
 
 class Solution:
     def isTimeValue(self, t1, t2, t3, t4):
-        if t1 < t2 and t3 > t4 and ((t3 <= t2 <= 24 or 0 < t2 <= t4) or (t3 <= t1 <= 24 or 0 < t1 <= t4)):
-            if t1 < t3 and t2 > t4:
-                return t2 - t3
-            elif t1 > t3 and t2 > t4:
-                return t2 - t1
-            elif t1 < t3 and t2 < t4:
-                return t2 - t1
-
-        elif t1 < t2 and t3 < t4 and ((t3 <= t2 <= t4) or (t3 <= t1 <= t4)):
-            if t1 > t3 and t2 < t4:
-                return t2 - t1
-            elif t1 < t3 and t2 < t4:
-                return t2 - t3
-            elif t1 > t3 and t2 > t4:
-                return t4 - t1
-            else:
-                t4 - t2
-        elif t1 > t2 and t3 < t4 and ((t3 <= t2 <= 24 and 0 < t2 <= t4) or (t3 <= t1 <= 24 and 0 < t1 <= t4)):
-            if t2 > t3 and t1 < t4:
-                return t2 - t1
-            elif t2 > t3 and t1 > t4:
-                return t2 - t4
-        elif t1 > t2 and t3 > t4 and ((t2 <= t4) or (t1 >= t3)):
-            if t2 < t4 and t1 > t3:
-                return 24 - t1 + t2
-            elif t2 < t4 and t1 < t3:
-                return 24 - t3 + t2
-            elif t2 > t4 and t1 < t3:
-                return 24 - t3 + t4
-            elif t2 > t4 and t1 > t3:
-                return 24 - t1 + t4
-
+        if t1 >= t2 and t3 >= t4:
+            print(111, t1,t2,t3,t4)
+            if t3 >= t2 >= t4 or t3 >= t1 >= t4:
+                t2 += 24
+                t4 += 24
+                l = [t1, t2, t3, t4]
+                l.sort()
+                print(l)
+                return l[2] - l[1]
+        if t1 >= t2 and t3 <= t4:
+            print(222, t1,t2,t3,t4)
+            if t3 <= t2 <= t4 or t3 <= t1 <= t4:
+                t2 += 24
+                t3 += 24
+                t4 += 24
+                l = [t1, t2, t3, t4]
+                l.sort()
+                print(l)
+                return l[2] - l[1]
+        if t1 <= t2 and t3 <= t4:
+            print(333, t1,t2,t3,t4)
+            if t3 <= t2 <= t4 or t3 <= t1 <= t4:
+                l = [t1, t2, t3, t4]
+                l.sort()
+                print(l)
+                return l[2] - l[1]
+        if t1 <= t2 and t3 >= t4:
+            d = 0
+            if t2 > t3:
+                d += t2 -t3
+            if t1 < t4:
+                d += t4 - t1
+            return d
 
 result = Solution()
-# t1, t2, t3, t4 = 8.0, 12.0, 22.0, 6.0
-# t1, t2, t3, t4 = 5.0, 22.0, 6.0, 23.0
-# t1, t2, t3, t4 = 2.0, 9.0, 6.4, 23.23
-# t1, t2, t3, t4 = 8.0, 22.0, 21.0, 6.0
-# t1, t2, t3, t4 = 22.0, 8.0, 21.0, 6.0
-# t1, t2, t3, t4 = 15.0, 24.0, 22.0, 6.0
-# t1, t2, t3, t4 = 1.0, 2.0, 22.0, 6.0
-# t1, t2, t3, t4 = 23.0, 2.0, 22.0, 6.0
-t1, t2, t3, t4 = 15.0, 3.0, 22.0, 6.0
+# t1, t2, t3, t4 = 8.0, 12.0, 22.0, 6.0 # none
+# t1, t2, t3, t4 = 5.0, 22.0, 6.0, 23.0 # 16
+# t1, t2, t3, t4 = 2.0, 9.0, 6.4, 23.23 # 2.6
+# t1, t2, t3, t4 = 8.0, 22.0, 21.0, 6.0 # 1 !!
+# t1, t2, t3, t4 = 4.0, 22.0, 21.0, 6.0 # 1 !!
+# t1, t2, t3, t4 = 21.0, 22.0, 20.0, 6.0 # 1 !!
+# t1, t2, t3, t4 = 22.0, 8.0, 21.0, 6.0 # 8
+# t1, t2, t3, t4 = 15.0, 24.0, 22.0, 6.0 # 2
+# t1, t2, t3, t4 = 1.0, 2.0, 22.0, 6.0 # 1 !!
+# t1, t2, t3, t4 = 7.0, 24.0, 22.0, 6.0 # 15 !!
+# t1, t2, t3, t4 = 7.0, 20.0, 22.0, 6.0 # 15 !!
+# t1, t2, t3, t4 = 23.0, 2.0, 22.0, 6.0 # 3
+# t1, t2, t3, t4 = 22.0, 1.0, 4.0, 1.0 # 8
+# t1, t2, t3, t4 = 22.0, 2.0, 22.0, 1.0 # 8
+# t1, t2, t3, t4 = 14.0, 8.0, 22.0, 6.0 # 8
+# t1, t2, t3, t4 = 4.0, 8.0, 22.0, 6.0 # 2
+# t1, t2, t3, t4 = 0.0, 15.0, 22.0, 6.0 # 6
+# t1, t2, t3, t4 = 0.0, 15.0, 15.0, 6.0 # 6
+# t1, t2, t3, t4 = 0.0, 15.0, 0.0, 14.0 # 14
 
 print(result.isTimeValue(t1, t2, t3, t4))
