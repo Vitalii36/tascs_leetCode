f0e241f415740738e32f2534e6d8404a3f017904
Pravdych Vitalii <vitaliipravdych@gmail.com>
2023-01-01 19:19:57 +0200

    [UPD] Ex_40_combination_sum_II

---
+++ Python/Ex_40_combination_sum_II.py
@@ -0,0 +1,65 @@
+"""
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in
+candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
+
+
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output:
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output:
+[
+[1,2,2],
+[5]
+]
+
+
+Constraints:
+
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
+"""
+
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def backtrack(comb, remain, curr, results):
+
+            if remain == 0:
+                results.append(list(comb))
+                return
+
+            for next_curr in range(curr, len(candidates)):
+
+                if next_curr > curr and candidates[next_curr] == candidates[next_curr - 1]:
+                    continue
+
+                pick = candidates[next_curr]
+                if remain - pick < 0:
+                    break
+
+                comb.append(pick)
+                backtrack(comb, remain - pick, next_curr + 1, results)
+                comb.pop()
+
+        candidates.sort()
+        comb, results = [], []
+        backtrack(comb, target, 0, results)
+
+        return results
